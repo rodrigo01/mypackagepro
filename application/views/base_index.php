588b7a0af3561b704626e1ebdb8b18fbc4d6c350
Rodrigo Ornelas <mailbox@rodrigo.mx>
2014-03-03 23:29:24 -0800

    Config modules and helpers
    set the boostrap

--- application/views/base_index.php
+++ application/views/base_index.php
@@ -2,7 +2,10 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Mypackage</title>
+	<link href="<?php echo base_url();?>css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 
@@ -11,5 +14,8 @@
 	<h1>Estamos haciendo cambios a nuestro sitio web, volveremos en unas horas!</h1>
 </div>
 
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="<?php echo base_url();?>js/bootstrap.min.js"></script>
 </body>
 </html>
