a38bcfe8e5cfc90dbb721a2e83c1b19186866dec
Rodrigo Ornelas <mailbox@rodrigo.mx>
2014-03-03 23:18:57 -0800

    Welcome Nuevo

---
+++ application/views/base_index.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Mypackage</title>
+</head>
+<body>
+
+<div id="container">
+	<h1>We are making changes to our website, will be back in a few hours!</h1>
+	<h1>Estamos haciendo cambios a nuestro sitio web, volveremos en unas horas!</h1>
+</div>
+
+</body>
+</html>
